1be16ba6aa239e2b6d518a0350b5acf723f61587
francopoffo <113190523+francopoffo@users.noreply.github.com>
2022-12-10 17:18:37 -0300

    Add files via upload

---
+++ Sum of Pairs/main.js
@@ -0,0 +1,8 @@
+function sumPairs(ints, s){
+    var seen = {}
+    for (var i = 0; i < ints.length; ++i) {
+      if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+      seen[ints[i]] = true
+    }
+  }
+   
